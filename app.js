2c03c2e9bc63c68164a8ee337534f50b63b2ad34
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-08-17 16:19:53 +0530

    correct all the errors in code and data inserted successfully in DB

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
  const app = express();
  const mongoose = require("mongoose");
- const Listing = require("../models/listing.js");
+ const Listing = require("./models/listing.js");
+ const path = require("path");
+
 
  const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -22,19 +24,19 @@ async function main() {
     res.send("Hi, i am Root");
  })
 
- app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "My New Villa",
-    description: "By the beach",
-    price: 1200,
-    location: "Calangute, Goa",
-    country: "India",
-  });
+//  app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
 
-  await sampleListing.save();
-  console.log("sample was saved");
-  res.send("successful testing");
-});
+//   await sampleListing.save();
+//   console.log("sample was saved");
+//   res.send("successful testing");
+// });
 
   app.listen(8080,(req,res)=>{
     console.log("Server is Runing on port 8080");
