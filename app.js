b6ea1a8c819c69ab18ab0d4a84109afbf792ecf9
Ganesh Machale <ganeshmachale2006@gmail.com>
2026-08-17 15:31:08 +0530

    started from the strach because there are lot of errors

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
+ const app = express();
+ const mongoose = require("mongoose");
 
+ app.get("/",(req,res)=>{
+    res.send("Hi, i am Root");
+ })
 
-  let port = 8080;
-  let Mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
-
-  main()
-  .then(()=>{
-    console.log("successfully Connected to DB");
-  })
-  .catch((err)=>{
-    console.log(err);
-  })
-  async function main(){
-    await mongoose.connect(Mongo_url);
-  }
-
-  app.listen(port,(req,res)=>{
-     console.log("Port is Runing on :",port);
+  app.listen(8080,(req,res)=>{
+    console.log("Server is Runing on port 8080");
   });
